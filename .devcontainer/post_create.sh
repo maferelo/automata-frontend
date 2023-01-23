646c393100332772f7600e36cf9c1c26e1ba3ed6
Felipe <maferelo@outlook.com>
2023-01-23 05:44:44 +0000

    lint and postcreate sh lint

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -1,4 +1,6 @@
+#!/bin/bash
+
 # Fix git dubious ownership error
-sudo git config --global --add safe.directory /app
+git config --global --add safe.directory /app
 # Fix git unable to undo changes
 git config core.filemode false
