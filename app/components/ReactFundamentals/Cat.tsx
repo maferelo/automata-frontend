e96751d85f4e422dcb0db77489634fb0092843e9
Felipe <maferelo@outlook.com>
2023-01-24 13:38:51 +0000

    react fundamentals listviews and some deps

--- app/components/ReactFundamentals/Cat.tsx
+++ app/components/ReactFundamentals/Cat.tsx
@@ -1,5 +1,14 @@
 import { useState } from "react";
-import { Button, ScrollView, Text, TextInput, View } from "react-native";
+import {
+  Button,
+  FlatList,
+  Platform,
+  ScrollView,
+  SectionList,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
 type CatProps = {
   name: string;
@@ -8,6 +17,13 @@ type CatProps = {
 const Cat = (props: CatProps) => {
   const [text, setText] = useState("");
   const [isHungry, setIsHungry] = useState(true);
+  const data: { key: string }[] = [
+    { key: "Devin" },
+    { key: "Devin2" },
+    { key: "Devin3" },
+    { key: "Devin4" },
+    { key: "Devin5" },
+  ];
 
   return (
     <ScrollView>
@@ -28,7 +44,9 @@ const Cat = (props: CatProps) => {
           onChangeText={(newText) => setText(newText)}
           defaultValue={text}
         />
-        <Text style={{ padding: 10, fontSize: 42 }}>
+        <Text
+          style={{ padding: Platform.OS === "ios" ? 30 : 10, fontSize: 42 }}
+        >
           {text
             .split(" ")
             .map((word) => word && "🍕")
@@ -44,6 +62,41 @@ const Cafe = () => {
     <>
       <Cat name="Munkustrap" />
       <Cat name="Spot" />
+      <FlatList
+        data={[
+          { key: "Devin" },
+          { key: "Dan" },
+          { key: "Dominic" },
+          { key: "Jackson" },
+          { key: "James" },
+          { key: "Joel" },
+          { key: "John" },
+          { key: "Jillian" },
+          { key: "Jimmy" },
+          { key: "Julie" },
+        ]}
+        renderItem={({ item }) => <Text>{item.key}</Text>}
+      />
+      <SectionList
+        sections={[
+          { title: "D", data: ["Devin", "Dan", "Dominic"] },
+          {
+            title: "J",
+            data: [
+              "Jackson",
+              "James",
+              "Jillian",
+              "Jimmy",
+              "Joel",
+              "John",
+              "Julie",
+            ],
+          },
+        ]}
+        renderItem={({ item }) => <Text>{item}</Text>}
+        renderSectionHeader={({ section }) => <Text>{section.title}</Text>}
+        keyExtractor={(item) => `basicListEntry-${item}`}
+      />
     </>
   );
 };
