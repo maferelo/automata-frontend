f69c58e0145396174d30b608cdd0a3902a24f558
Felipe <maferelo@outlook.com>
2023-02-23 13:46:10 +0000

    e2e: First home test

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,17 +1,6 @@
-describe("My First Test", () => {
-  it("Gets, types and asserts", () => {
-    cy.visit("https://example.cypress.io");
-
-    cy.contains("type").click();
-
-    // Should be on a new URL which
-    // includes '/commands/actions'
-    cy.url().should("include", "/commands/actions");
-
-    // Get an input, type into it
-    cy.get(".action-email").type("fake@email.com");
-
-    //  Verify that the value has been updated
-    cy.get(".action-email").should("have.value", "fake@email.com");
+describe("Home Page", () => {
+  it("Visits the app root url", () => {
+    cy.visit("/");
+    cy.contains("Go to notifications").click();
   });
 });
